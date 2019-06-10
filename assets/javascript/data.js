c992a220db915877e647dfac58f3e2ebc6e2f3fe
sharryheb <43225557+sharryheb@users.noreply.github.com>
2019-06-10 02:07:31 -0700

    finishing the game

---
+++ assets/javascript/data.js
@@ -0,0 +1,233 @@
+let moviesData = [
+    {
+        title: "Splash",
+        clip: "https://www.youtube.com/embed/NwrQ5iKmkGU?autoplay=1",
+        year: "1984",
+        thCharacter: "Allen Bauer"
+    },
+
+    {
+        title: "Bachelor Party",
+        clip: "https://www.youtube.com/embed/-lxTcw_o6ek?autoplay=1",
+        year: "1984",
+        thCharacter: "Rick Gassko"
+    },
+
+    {
+        title: "The Man With One Red Shoe",
+        clip: "https://www.youtube.com/embed/SIQtE1ND_oA?autoplay=1",
+        year: "1985",
+        thCharacter: "Richard Drew"
+    },
+
+    {
+        title: "The Money Pit",
+        clip: "https://www.youtube.com/embed/qgPHwLSMEiY?autoplay=1",
+        year: "1986",
+        thCharacter: "Walter Fielding"
+    },
+
+    {
+        title: "Dragnet",
+        clip: "https://www.youtube.com/embed/nvCoaBme8Jo?autoplay=1",
+        year: "1987",
+        thCharacter: "Det. Pep Streebek"
+    },
+
+    {
+        title: "Big",
+        clip: "https://www.youtube.com/embed/CF7-rz9nIn4?autoplay=1",
+        year: "1988",
+        thCharacter: "Josh Baskin"
+    },
+
+    {
+        title: "Punchline",
+        clip: "https://www.youtube.com/embed/h8xc6j7i83I?autoplay=1",
+        year: "1988",
+        thCharacter: "Steven Gold"
+    },
+
+    {
+        title: "The Burbs",
+        clip: "https://www.youtube.com/embed/eCfvE03ufF8?autoplay=1",
+        year: "1989",
+        thCharacter: "Ray Peterson"
+    },
+
+    {
+        title: "Turner and Hooch",
+        clip: "https://www.youtube.com/embed/bqDf22JiTyw?autoplay=1",
+        year: "1989",
+        thCharacter: "Scott Turner"
+    },
+
+    {
+        title: "Joe Versus The Volcano",
+        clip: "https://www.youtube.com/embed/xcYdOQSVH5c?autoplay=1",
+        year: "1990",
+        thCharacter: "Joe Banks"
+    },
+
+    {
+        title: "A League of Their Own",
+        clip: "https://www.youtube.com/embed/6M8szlSa-8o?autoplay=1",
+        year: "1992",
+        thCharacter: "Jimmy Dugan"
+    },
+
+    {
+        title: "Sleepless in Seattle",
+        clip: "https://www.youtube.com/embed/c77JrXbqqV0?autoplay=1",
+        year: "1993",
+        thCharacter: "Sam Baldwin"
+    },
+
+    {
+        title: "Philadelphia",
+        clip: "https://www.youtube.com/embed/wHSH-NpCQOw?autoplay=1",
+        year: "1993",
+        thCharacter: "Andrew Beckett"
+    },
+
+    {
+        title: "Forrest Gump",
+        clip: "https://www.youtube.com/embed/CJh59vZ8ccc?autoplay=1",
+        year: "1994",
+        thCharacter: "Forrest Gump"
+    },
+
+    {
+        title: "Toy Story",
+        clip: "https://www.youtube.com/embed/HF70FZbW8rY?autoplay=1",
+        year: "1995",
+        thCharacter: "Woody"
+    },
+
+    {
+        title: "Saving Private Ryan",
+        clip: "https://www.youtube.com/embed/LvAIBDGIYE0?autoplay=1",
+        year: "1998",
+        thCharacter: "Captain Miller"
+    },
+
+    {
+        title: "You've Got Mail",
+        clip: "https://www.youtube.com/embed/WXLPNi3xLDc?autoplay=1",
+        year: "1998",
+        thCharacter: "Joe Fox"
+    },
+
+    {
+        title: "The Green Mile",
+        clip: "https://www.youtube.com/embed/4uDhwFU9unc?autoplay=1",
+        year: "1999",
+        thCharacter: "Paul Edgecomb"
+    },
+
+    {
+        title: "Cast Away",
+        clip: "https://www.youtube.com/embed/3c5Q7qbcwKQ?autoplay=1",
+        year: "2000",
+        thCharacter: "Chuck Noland"
+    },
+
+    {
+        title: "Road to Perdition",
+        clip: "https://www.youtube.com/embed/O_4Sx5NtOPM?autoplay=1",
+        year: "2002",
+        thCharacter: "Michael Sullivan"
+    },
+
+    {
+        title: "Catch Me If You Can",
+        clip: "https://www.youtube.com/embed/KAeAqaA0Llg?autoplay=1",
+        year: "2002",
+        thCharacter: "Carl Hanratty"
+    },
+
+    {
+        title: "The Ladykillers",
+        clip: "https://www.youtube.com/embed/JWOYQ5lNK8U?autoplay=1",
+        year: "2004",
+        thCharacter: "Professor G.H. Dorr"
+    },
+
+    {
+        title: "The Terminal",
+        clip: "https://www.youtube.com/embed/iFgF2rN1f4o?autoplay=1",
+        year: "2004",
+        thCharacter: "Viktor Navorski"
+    },
+
+    {
+        title: "The Polar Express",
+        clip: "https://www.youtube.com/embed/Cht63QybtiA?autoplay=1",
+        year: "2004",
+        thCharacter: "Hero Boy / Father / Conductor / Hobo / Scrooge / Santa Claus"
+    },
+
+    {
+        title: "The DaVinci Code",
+        clip: "https://www.youtube.com/embed/H8iT85l5l6E?autoplay=1",
+        year: "2006",
+        thCharacter: "Robert Langdon"
+    },
+
+    {
+        title: "Charlie Wilson's War",
+        clip: "https://www.youtube.com/embed/-c_ctZ4lUCk?autoplay=1",
+        year: "2007",
+        thCharacter: "Charlie Wilson"
+    },
+
+    {
+        title: "Angels & Demons",
+        clip: "https://www.youtube.com/embed/LxxFi7igv2A?autoplay=1",
+        year: "2009",
+        thCharacter: "Robert Langdon"
+    },
+
+    {
+        title: "Captain Phillips",
+        clip: "https://www.youtube.com/embed/LxxFi7igv2A?autoplay=1",
+        year: "2013",
+        thCharacter: "Captain Richard Phillips"
+    },
+
+    {
+        title: "Saving Mr. Banks",
+        clip: "https://www.youtube.com/embed/b49dP4yGFzA?autoplay=1",
+        year: "2013",
+        thCharacter: "Walt Disney"
+    },
+
+    {
+        title: "Sully",
+        clip: "https://www.youtube.com/embed/68wP3J8V1ZA?autoplay=1",
+        year: "2016",
+        thCharacter: "Chesley 'Sully' Sullenberger"
+    },
+
+    {
+        title: "The Circle",
+        clip: "https://www.youtube.com/embed/IwoadnWKs40?autoplay=1",
+        year: "2017",
+        thCharacter: "Eamon Bailey"
+    },
+
+    {
+        title: "Inferno",
+        clip: "https://www.youtube.com/embed/2ycw0UUyCm0?autoplay=1",
+        year: "2016",
+        thCharacter: "Robert Langdon"
+    },
+
+    {
+        title: "The Post",
+        clip: "https://www.youtube.com/embed/kSxin-vsnmw?autoplay=1",
+        year: "2017",
+        thCharacter: "Ben Bradlee"
+    },
+];
+
---
+++ assets/javascript/game.js
@@ -0,0 +1,125 @@
+
+var wins = 0;
+
+var Hangman =
+{
+    initializeGame: function()
+    {
+        this.guess = "";
+        this.guessCount = 10;
+        this.answer = moviesData[Math.floor(Math.random() * moviesData.length)];
+        this.guessHistory = [];
+        this.gameOver = false;
+        this.gameAnswer = this.answer.title.toLowerCase().split("");
+        this.display = this.answer.title.split("");
+        for (var i=0; i<this.display.length; i++)
+        {
+            var currentLetter = this.display[i];
+            if (currentLetter != " " && currentLetter != "'" && currentLetter != "," && currentLetter != "." && currentLetter != "?" && currentLetter != "!" && currentLetter != "&")
+            {
+                this.display[i] = "_";
+            }
+            else if (this.display[i] === " ")
+            {
+                this.display[i] = "~~~"; 
+                this.gameAnswer[i] = "~~~"; 
+            }
+        }
+
+        this.display = this.display.join(" ").replace(/~/gi, "&nbsp;");
+        this.gameAnswer = this.gameAnswer.join(" ").replace(/~/gi, "&nbsp;");
+        this.renderResults();
+        /*
+        document.getElementById("result").innerHTML = this.display;
+        document.getElementById("winTotal").innerHTML = "# of Wins: " + wins;
+        document.getElementById("status").innerHTML = "Good Luck!";
+        document.getElementById("pastGuesses").innerHTML = "";
+        document.getElementById("guessesRemaining").innerHTML = this.guessCount;
+        */
+    },
+
+    processGuess: function()
+    {   
+        this.display = this.display.split("");
+        this.gameAnswer = this.gameAnswer.split("");
+
+        for(var i=0; i < this.gameAnswer.length; i++)
+        {
+            if (this.guess == this.gameAnswer[i].toLowerCase())
+            {
+                this.display[i] = this.display[i].replace('_', this.gameAnswer[i]);
+            }
+        }
+
+        this.display = this.display.join("");
+        this.gameAnswer = this.gameAnswer.join("")
+
+        return this.display;
+    },
+
+    renderResults: function()
+    {
+        document.getElementById("result").innerHTML = this.display;
+        document.getElementById("pastGuesses").innerHTML = this.guessHistory.join(" ");
+        document.getElementById("guessesRemaining").innerHTML = this.guessCount;
+
+        
+        if (this.display.indexOf("_") < 0) 
+        {
+            wins++;
+            document.getElementById("status").innerHTML = "YOU WIN!";
+            this.gameOver = true;
+        }
+         
+        else if (this.guessCount <= 0)
+        {
+            document.getElementById("status").innerHTML = "Game Over! Sorry, Try Again!";
+            this.gameOver = true;
+        }
+        else
+        {
+            document.getElementById("status").innerHTML = "Good Luck!";
+        }
+
+        if (this.gameOver)
+        {
+            document.getElementById("winTotal").innerHTML = "# of Wins: " + wins;
+            document.getElementById("movieClip").src = this.answer.clip;
+            document.getElementById("movieTitle").textContent = this.answer.title;
+            document.getElementById("movieText").textContent = this.answer.year + ". Tom Hanks played " + this.answer.thCharacter + ".";
+            document.getElementById("resultCard").style.visibility = "visible";
+        }
+    },
+
+    gameControl: function(event)
+    {
+        if (this.guessCount >= 0)
+        {
+            this.guess = event.key;
+
+            if (this.guessHistory.indexOf(this.guess.toUpperCase()) < 0) 
+            {
+                this.guessHistory.push(this.guess.toUpperCase());
+                this.guess = this.guess.toLowerCase();
+
+                this.display = this.processGuess();
+
+                if (this.gameAnswer.indexOf(this.guess) < 0)
+                    this.guessCount--;
+                
+                this.renderResults();
+            }
+            else 
+            {
+                document.getElementById("status").innerHTML = "You already guessed that letter - try again."
+            }
+        }
+        
+    }
+}
+
+
+
+
+
+
